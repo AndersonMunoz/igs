ebc50dcb2f8d7719312840fcc2257b440aa0e384
AndersonMunoz <anderson180@hotmail.es>
2023-05-31 17:48:32 -0500

    Input radio corregido

--- js/formuAnderson.js
+++ js/formuAnderson.js
@@ -11,13 +11,26 @@ form.addEventListener('submit', function(event) {
       form.reset(); // Opcionalmente puedes restablecer los campos del formulario
     }
   });
-
-  // Mostrar el botón de registro cuando se completen todos los campos requeridos
-  form.addEventListener('input', function() {
-    registerButton.style.display = form.checkValidity() ? 'block' : 'none';
-  });
-
   // Agregar evento al botón de cancelar para redirigir a otra página
   cancelButton.addEventListener('click', function() {
     window.location.href = 'modelo-anderson.html'; // Reemplaza 'otra_pagina.html' con la URL de la página a la que deseas redirigir
   });
+// Mostrar el botón de registro cuando se completen todos los campos requeridos
+form.addEventListener('input', function() {
+  const requiredFields = Array.from(form.querySelectorAll('input[required]'));
+  const radioFields = Array.from(form.querySelectorAll('input[type="radio"]'));
+
+  // Verificar si todos los campos requeridos están completos
+  const allFieldsCompleted = requiredFields.every(field => field.value.trim() !== '');
+
+  // Verificar si al menos uno de los campos de tipo radio está seleccionado
+  const radioSelected = radioFields.some(field => field.checked);
+
+  if (allFieldsCompleted || radioSelected) {
+    registerButton.style.display = 'inline-block';
+  } else {
+    registerButton.style.display = 'none';
+  }
+});
+
+
