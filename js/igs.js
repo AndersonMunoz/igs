a3a8d8e28b6369cbe0035b7b5d065602807dc024
medina10-01 <mstiven748@gmail.com>
2023-06-14 10:37:57 -0500

    datos incompletos (modal alert)

--- js/igs.js
+++ js/igs.js
@@ -11,15 +11,17 @@ let Surname = document.getElementById('Surname');
 let Email = document.getElementById('Email');
 let Pass = document.getElementById('passWord');
 let userType = document.getElementById('userType');
+let eror = document.getElementById('modalAlertFalse');
 
 let notnom = document.getElementById('notName');
 let notapl = document.getElementById('notSurName');
-let notMail = document.getElementById('notSurName');
+let notMail = document.getElementById('notEmail');
 let notPass = document.getElementById('notPassWord');
+let user = document.getElementById('notUser');
+let btnaceptar = document.getElementById('btnAceptar');
 
 
 mian.addEventListener('click', function () {
-
     if (mini.classList.contains('none')) {
         mini.classList.add('menu');
         mini.classList.remove('none');
@@ -45,19 +47,46 @@ btnNew.addEventListener('click', function add() {
 });
 btnReg.addEventListener('click', function () {
     if (nombre.value.length === 0) {
-        alert ('te falta el nombre');
-    }else if (Surname.value.length === 0) {
-        alert ('falta los apellidos');
-    }else if (Email.value.length === 0) {
-        alert('te falta el email');
-    }else if (Pass.value.length === 0) {
-        alert ('falta la contraseña');
-    }else if (userType.value == 0) {
-        alert ('tipo de usuario');
+        eror.classList.add('modal-alert');
+        eror.classList.remove('none');
+        notnom.style.visibility = 'visible'
+    }else {
+        notnom.style.visibility = 'hidden'
+    }
+    if (Surname.value.length === 0) {
+        eror.classList.add('modal-alert');
+        eror.classList.remove('none');
+        notapl.style.visibility = 'visible'
+    }else {
+        notapl.style.visibility = 'hidden'
+    }
+    if (Email.value.length === 0) {
+        eror.classList.add('modal-alert');
+        eror.classList.remove('none');
+        notMail.style.visibility = 'visible'
+    }else {
+        notMail.style.visibility = 'hidden'
+    }
+    if (Pass.value.length === 0) {
+        eror.classList.add('modal-alert');
+        eror.classList.remove('none');
+        notPass.style.visibility = 'visible'
+    }else {
+        notPass.style.visibility = 'hidden'
+    }
+    if (userType.value == 0) {
+        eror.classList.add('modal-alert');
+        eror.classList.remove('none');
+        user.style.visibility = 'visible'
+    }else {
+        user.style.visibility = 'hidden'
     }
     if ((nombre.value.length !== 0)&&(Email.value.length !== 0)&&(Pass.value.length !== 0)&&(userType.value != 0)) {
         alerta.classList.add('modal-alert');
         alerta.classList.remove('none');
     }
-
 });
+btnaceptar.addEventListener('click', function () {
+    eror.classList.remove('modal-alert');
+    eror.classList.add('none');
+})
