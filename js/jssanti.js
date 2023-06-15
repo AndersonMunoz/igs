51207e5e8be9f16dc2109c108bd96376aaddc733
ersanti14 <erazoser14@gmail.com>
2023-06-15 00:30:31 -0500

    commit

--- js/jssanti.js
+++ js/jssanti.js
@@ -3,20 +3,63 @@ let documentoTxt = document.getElementById('documento');
 let nombreTxt = document.getElementById('nombre');
 let apellidoTxt = document.getElementById('apellido');
 let btnEnviar = document.getElementById('enviar');
+let modalCor = document.getElementById('modal');
+let cerrar = document.getElementById('cerrarModal');
+let modalCor2 = document.getElementById('modal2');
+let cerrar2 = document.getElementById('cerrarModal2');
 
 btnEnviar.addEventListener("click", function() {
-    if (apellidoTxt.value.length === 0){
+    if (documentoTxt.value.length === 0 && " "){
 
-        
-        alert("Este campo es requerido, porfavor completar")
+        modalCor.classList.add('modal--show');
+        modalCor.classList.remove('modal');
+
+        cerrar.addEventListener("click",function(){
+            modalCor.classList.remove('modal--show')
+        modalCor.classList.add('modal')
+        })
+    }
+    if(nombreTxt.value.length === 0 && " "){
+
+        modalCor.classList.add('modal--show');
+        modalCor.classList.remove('modal');
+
+        cerrar.addEventListener("click",function(){
+            modalCor.classList.remove('modal--show')
+        modalCor.classList.add('modal')
+        })
+    }
+    if(apellidoTxt.value.length === 0 && " "){
+
+        modalCor.classList.add('modal--show');
+        modalCor.classList.remove('modal');
+
+        cerrar.addEventListener("click",function(){
+            modalCor.classList.remove('modal--show')
+        modalCor.classList.add('modal')
+        })
     }
     else{
-        alert("bienvendido")
+        modalCor2.classList.add('modal--show2');
+        modalCor2.classList.remove('modal2');
+
+        cerrar2.addEventListener("click",function(){
+        modalCor2.classList.remove('modal--show2')
+        modalCor2.classList.add('modal2')
+        })
     }
 });
 
+cerrar.addEventListener("click",function(){
+            modalCor.classList.remove('modal--show')
+        modalCor.classList.add('modal')
+        })
 
-
+/* cerrar2.addEventListener("click",function(){
+            modalCor2.classList.remove('modal--show2')
+        modalCor2.classList.add('modal2')
+        })
+ */
 
 
 
