4504e75d7c5f037ab7ffd6c61f8aee644177d8f4
Brayan <mstiven748@gmail.com>
2023-10-04 17:35:57 -0500

    ultimo cambio

--- backend/src/database/conexion.js
+++ backend/src/database/conexion.js
@@ -3,18 +3,10 @@ import dotenv from 'dotenv';
 dotenv.config({path:'./env/.env'});
 
 
-    export const pool = createPool({
-         host: 'localhost', 
-         user: 'root',      
-         password: '',      
-         port: 3306,        
-         database: 'igs'    
+export const pool = createPool({
+        host:process.env.DB_HOST,
+        user:process.env.DB_USER,
+        password:process.env.DB_PASSWORD,
+        port:process.env.DB_PORT,
+        database:process.env.DB_DATABASE
 })
-
-// export const pool = createPool({
-//         host:process.env.DB_HOST,
-//         user:process.env.DB_USER,
-//         password:process.env.DB_PASSWORD,
-//         port:process.env.DB_PORT,
-//         database:process.env.DB_DATABASE
-// })
