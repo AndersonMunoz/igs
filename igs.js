5b046983bf2f4a9529b1016ad88c377a3a9dc994
karenviviana1234 <karenvivianadiazguevara@gmil.com>
2023-10-04 11:44:52 -0500

    crudProducto

--- igs.js
+++ igs.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import body_parser from  'body-parser';
+import productoRouter from './backend/src/routes/productos.routes.js'
 
 const igs = express();
 
 igs.use(body_parser.json());
 igs.use(body_parser.urlencoded({extended:false}));
 
+
+igs.use('/producto',productoRouter);
 
 
 igs.listen(3000,()=>{
