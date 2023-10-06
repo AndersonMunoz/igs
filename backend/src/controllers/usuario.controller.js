fba3e6ad9224056a6d396c1cfa67cfe3c0569eac
ersanti14 <erazoser14@gmail.com>
2023-10-05 22:30:29 -0500

    funcionando al 100 con validacion

--- backend/src/controllers/usuario.controller.js
+++ backend/src/controllers/usuario.controller.js
@@ -1,9 +1,16 @@
 import { pool } from "../database/conexion.js"
+import { validationResult } from "express-validator";
 
 
 export const registroUsuario = async (req, res) => {
 
     try {
+
+        let error= validationResult(req);
+        if (!error.isEmpty()) {
+            return res.status(400).json(error)           
+        }
+
         let { documento, email, nombre, contraseña, tipo } = req.body;
         let sql = `insert into usuarios (documento_usuario,email_usuario,nombre_usuario, contrasena_usuario, tipo_usuario)
     values('${documento}','${email}','${nombre}','${contraseña}','${tipo}')`;
@@ -68,6 +75,36 @@ export const buscarUsuario = async (req, res) => {
     }
 
 };
+
+export const editarUsuario = async (req ,res) =>{
+    try {
+        let error= validationResult(req);
+        if (!error.isEmpty()) {
+            return res.status(400).json(error)           
+        }
+        let id = req.params.id;
+    let {documento, email, nombre, contraseña, tipo } = req.body;
+
+    let sql=`update usuarios SET documento_usuario = '${documento}',
+    email_usuario = '${email}', nombre_usuario = '${nombre}',contrasena_usuario = '${contraseña}',tipo_usuario = '${tipo}'
+    where id_usuario = ${id} `;
+    console.log(sql)
+
+    const [rows] = await pool.query(sql);
+    if (rows.affectedRows>0){
+        res.status(200).json(
+            {"status": 200,"menssge": "Se actualizo con exito el usuario"});
+    }else{
+        res.status(401).json(
+            {"status": 401,"menssge": "No se actualizo el usuario"});
+    }
+
+    } catch (e) {
+        res.status(500).json({
+        "status": 500,
+        "menssge": "Error interno en el sevidor :(" + e});
+    }
+}
 
 export const actualizarEstado = async (req,res) =>{
     try{
