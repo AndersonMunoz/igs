0e6fff9bb3c4f4ff89c107e435934a3cecb803a0
Brayan <mstiven748@gmail.com>
2023-10-30 17:23:00 -0500

    dwnload

---
+++ igs.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import body_parser from  'body-parser';
+
+// importar la ruta de provedor
+import provedorRouter from './backend/src/routes/provedor.routes.js'
+
+const igs = express();
+
+igs.use(body_parser.json());
+igs.use(body_parser.urlencoded({extended:false}));
+
+
+igs.use('/provedor', provedorRouter);
+
+
+
+
+
+
+
+igs.listen(3000,()=>{
+    console.log('Servidor IGS ejecutando en el puerto 3000');
+})
+
+
