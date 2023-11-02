38e2d935b0326c899ab1559e2240a353363258f8
DaricksonCh <darickson.stiven96@gmail.com>
2023-11-02 09:52:44 -0500

    quiensabeque

--- igs.js
+++ igs.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import body_parser from  'body-parser';
+import cors from 'cors';
 import facturaMovimientoRoute from './backend/src/routes/facturaMovimiento.routes.js';
 import productoRouter from './backend/src/routes/productos.routes.js';
 import provedorRouter from './backend/src/routes/provedor.routes.js';
@@ -11,6 +12,7 @@ const igs = express();
 
 igs.use(body_parser.json());
 igs.use(body_parser.urlencoded({extended:false}));
+igs.use(cors());
 
 igs.use('/facturamovimiento',facturaMovimientoRoute);
 
