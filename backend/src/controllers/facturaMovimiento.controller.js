ba8b57dfd22df7279bdc507130cf98616eb2559e
AndersonMunoz <anderson180@hotmail.es>
2023-11-09 17:20:51 -0500

    Resturando registro de movimiento

--- backend/src/controllers/facturaMovimiento.controller.js
+++ backend/src/controllers/facturaMovimiento.controller.js
@@ -1,6 +1,38 @@
 import { pool } from '../database/conexion.js';
 import { validationResult } from 'express-validator';
 
+export const guardarMovimiento = async (req,res)=>{
+	try{	
+		let error = validationResult(req);
+        if (!error.isEmpty()){
+            return res.status(400).json(error);
+        }
+			let{tipo_movimiento,cantidad_peso_movimiento,unidad_peso_movimiento,precio_movimiento,estado_producto_movimiento,nota_factura,fecha_caducidad_producto,fk_id_producto,fk_id_usuario,fk_id_proveedor} = req.body;
+			let sql=`INSERT INTO factura_movimiento (tipo_movimiento,cantidad_peso_movimiento,unidad_peso_movimiento,precio_movimiento,estado_producto_movimiento,nota_factura,fecha_caducidad_producto,fk_id_producto,fk_id_usuario,fk_id_proveedor)
+									VALUES ('${tipo_movimiento}','${cantidad_peso_movimiento}','${unidad_peso_movimiento}','${precio_movimiento}','${estado_producto_movimiento}','${nota_factura}','${fecha_caducidad_producto}','${fk_id_producto}','${fk_id_usuario}','${fk_id_proveedor}')`;
+			//console.log(sql); En caso de no servir la inserción, descomente esto
+			const [rows] = await pool.query(sql);
+			//console.log(rows);
+			if (rows.affectedRows>0){
+					res.status(200).json(
+							{
+								"status":200,
+								"message":"Se registró el movimiento :D "
+							}
+					)
+			} else {
+					res.status(401).json(
+							{
+									"status":401,
+									"message":"NO se registró el movimiento :("
+							}
+					)
+			}
+			console.log()
+	} catch (e){
+			res.status(500).json({"status":500,"message":"Error en el servidor"+e});
+	}
+};
 /* export const guardarMovimiento = async (req, res) => {
 	try {
 		let error = validationResult(req);
