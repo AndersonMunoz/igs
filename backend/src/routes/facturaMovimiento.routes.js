a9fbb20b25462c1b88dccb76286094758737a6ed
DaricksonCh <darickson.stiven96@gmail.com>
2023-11-21 17:58:56 -0500

    nose

--- backend/src/routes/facturaMovimiento.routes.js
+++ backend/src/routes/facturaMovimiento.routes.js
@@ -1,20 +1,12 @@
 import { Router } from "express";
 
 import {validarFacturaMovimiento} from '../validation/facturaMovimiento.validator.js';
-<<<<<<< HEAD
-import {listarMovimientos,buscarMovimiento,actualizarMovimiento} from '../controllers/facturaMovimiento.controller.js';
-
-const facturaMovimientoRoute = Router();
-
-facturaMovimientoRoute.post('/registrar',validarFacturaMovimiento);
-=======
 import {guardarMovimiento,listarMovimientos,buscarMovimiento,actualizarMovimiento} from '../controllers/facturaMovimiento.controller.js';
 import { validarToken } from "../controllers/autentificacion.controller.js";
 
 const facturaMovimientoRoute = Router();
 
 facturaMovimientoRoute.post('/registrar',validarToken, validarFacturaMovimiento,guardarMovimiento);
->>>>>>> development
 facturaMovimientoRoute.get('/listar', listarMovimientos);
 facturaMovimientoRoute.get('/buscar/:id',buscarMovimiento);
 facturaMovimientoRoute.put('/actualizar/:id', validarToken, validarFacturaMovimiento,actualizarMovimiento);
--- backend/src/routes/productos.routes.js
+++ backend/src/routes/productos.routes.js
@@ -5,20 +5,11 @@ import { validarToken } from "../controllers/autentificacion.controller.js";
 
 const productoRouter = Router();
 
-<<<<<<< HEAD
-productoRouter.post('/registrar',guardarProducto);
-productoRouter.get('/listar',listarProductos);
-productoRouter.get('/buscar/:id',buscarProducto);
-productoRouter.put('/actualizar/:id',actualizarProducto);
-productoRouter.patch('/deshabilitar/:id',deshabilitarProducto);
-
-=======
 productoRouter.post('/registrar', validarToken,validatorProducto, guardarProducto);
 /* productoRouter.get('/listar',listarProductos); */
 productoRouter.get('/buscar/:id',buscarProducto);
 productoRouter.put('/actualizar/:id',validarToken, validatorProducto,actualizarProducto);
 productoRouter.patch('/deshabilitar/:id', validarToken, deshabilitarProducto);
 productoRouter.get('/listar', listarProductos);
->>>>>>> development
 
 export default productoRouter;
