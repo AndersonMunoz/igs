dc8126a564ed077aa305217e766bfd0f6868941f
medina10-01 <mstiven748@gmail.com>
2023-12-01 11:28:38 -0500

    delate

--- frontendReact/src/main.jsx
+++ frontendReact/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.js'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
