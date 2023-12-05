bd32c9d0cc8b078814d8f55055f9d12e79488152
AndersonMunoz <anderson180@hotmail.es>
2023-12-05 18:29:40 -0500

    Errores mejorados

--- frontendReact/src/components/Categoria.jsx
+++ frontendReact/src/components/Categoria.jsx
@@ -7,35 +7,31 @@ const Categoria = () => {
     listarCategorias();
   }, []);
 
-  function listarMovimiento(){
-    fetch('http://localhost:3000/facturamovimiento/listar',{
-      method: "get",
-      headers:{
-        "content-type":"application/json"
-      }
+  function listarCategorias() {
+    fetch("http://localhost:3000/categoria/listar", {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+      },
     })
-    .then(res=>res.json())
-    .then(data=>{
-      console.log(data);
-      let row = '';
-      data.forEach(element => {
-        row += `<tr>
-                  <td>${element.fk_id_producto}</td>        
-                  <td>${element.fecha_movimiento}</td>        
-                  <td>${element.tipo_movimiento}</td>        
-                  <td>${element.cantidad_peso_movimiento_movimiento}</td>        
-                  <td>${element.unidad_peso_movimiento}</td>        
-                  <td>${element.precio_movimiento}</td>        
-                  <td>${element.estado_producto_movimientoo}</td>        
-                  <td>${element.nota_factura}</td>        
-                  <td>${element.fecha_caducidad}</td>        
-                  <td><a data-bs-toggle="modal" data-bs-target="#exampleModal2" href="javaScript:editarMovimiento(${element.id_factura})">Editar</a></td>           
-                </tr>`
-        document.getElementById('tableMovimiento').innerHTML = row;
+      .then((res) => res.json())
+      .then(data=>{
+        console.log(data);
+        let row = '';
+        data.forEach(element => {
+          row += `<tr>
+                <td>${element.id_categoria}</td>        
+                <td>${element.nombre_categoria}</td>        
+                <td><a href="javaScript:editarCategoria(${element.id_categoria})">Editar</a></td>           
+                <td><a href="javaScript:eliminarCategoria(${element.id_categoria})">Eliminar</a></td>           
+              </tr>`
+          document.getElementById('tableCatategoria').innerHTML = row;
+    });
+  })
+      .catch((e) => {
+        console.log(e);
       });
-    })
-    .catch(e => {console.log(e);})
-  };
+  }
 
   return (
     <>
