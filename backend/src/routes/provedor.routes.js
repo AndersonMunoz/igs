d5b6b306457fb2033a4745815050ef993584da07
medina10-01 <mstiven748@gmail.com>
2024-02-13 09:56:15 -0500

    listarPorveedorActivo

--- backend/src/routes/provedor.routes.js
+++ backend/src/routes/provedor.routes.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 
-import {listarProvedor,buscarProvedor,registrarProvedor,eliminarProvedor,actualizarProvedor} from '../controllers/provedor.controler.js';
+import {listarProvedor,buscarProvedor,registrarProvedor,eliminarProvedor,actualizarProvedor, listarProvedorActivo} from '../controllers/provedor.controler.js';
 import { validarToken } from "../controllers/autentificacion.controller.js";
 import { validarProvedor } from "../validation/validatorProvedor.js";
 
 const provedorRouter = Router();
 
 provedorRouter.get('/listar', listarProvedor);
+provedorRouter.get('/listarActivo', listarProvedorActivo);
 provedorRouter.get('/buscar/:id',buscarProvedor);
 provedorRouter.post('/registrar' ,validarProvedor,/*validarToken , */registrarProvedor);
 provedorRouter.put('/eliminar/:id' ,/* validarToken , */ eliminarProvedor);
