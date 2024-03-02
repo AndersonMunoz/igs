34bd23471b3ced8a4ae0ec9582b710f6967cf9c7
ersanti14 <erazoser14@gmail.com>
2024-03-01 19:55:49 -0500

    usuario funcional

--- frontendReact/src/components/PerfilAjustes.jsx
+++ frontendReact/src/components/PerfilAjustes.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "../style/ajustesCss.css";
 
 
 
@@ -11,8 +12,17 @@ const PerfilAjustes = () =>{
 
     return(
         <>
+
         <div className="containerAjustes">
-            <h1>ajustessssss</h1>
+            <div className="ajustesDiv">
+                <div className="imgPerfilajustes">
+                </div>
+
+            </div>
+            <div className="ajustesFormu">
+                
+
+            </div>
         </div>
         </>
     )
