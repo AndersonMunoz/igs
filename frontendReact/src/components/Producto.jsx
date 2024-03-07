ac974d02c4ab5556a4ec6c095eb6ef850d989103
karenviviana1234 <karenvivianadiazguevara@gmail.com>
2024-03-07 09:37:47 -0500

    order

--- frontendReact/src/components/Producto.jsx
+++ frontendReact/src/components/Producto.jsx
@@ -141,7 +141,7 @@ const Producto = () => {
            [ 10, 50, 100, -1 ],
            [ '10 Filas', '50 Filas', '100 Filas', 'Ver Todo' ]
         ],
-        order: [[0, 'desc']],
+        order: [[10, 'asc']],
      });
 		}
 	}, [productos]);
