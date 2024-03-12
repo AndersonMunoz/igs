06ebedaf6ee2788e98ee61f235db0a44a2150340
medina10-01 <mstiven748@gmai.com>
2024-03-12 18:26:19 -0500

    goBack

--- frontendReact/src/const/portConexion.jsx
+++ frontendReact/src/const/portConexion.jsx
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
- const portConexion = '192.168.0.111';
-=======
 const portConexion = "10.193.129.25";
->>>>>>> 21b6a3111813a97fc51dbf1eb6f37a395ef272b5
 
 export default portConexion;
