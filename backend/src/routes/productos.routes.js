7fd54d301032372922e8683f553577e30a9d8c19
medina10_01 <mstiven748@gmail.com>
2024-03-14 10:31:48 -0500

    f

--- backend/src/routes/productos.routes.js
+++ backend/src/routes/productos.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import {guardarProducto,listarProductos,buscarProducto,actualizarProducto,deshabilitarProducto,activarProducto} from '../controllers/productos.controller.js';
+import {guardarProducto,listarProductos,buscarProducto,actualizarProducto,deshabilitarProducto,activarProducto,obtenerValorTotalProductosFecha} from '../controllers/productos.controller.js';
 import {validatorProducto} from '../validation/producto.validator.js'
 import { validarToken } from "../controllers/autentificacion.controller.js";
 
@@ -7,6 +7,7 @@ const productoRouter = Router();
 
 productoRouter.post('/registrar', validarToken,validatorProducto,  guardarProducto);
 productoRouter.get('/listar',validarToken,listarProductos);
+productoRouter.get('/listarProductoTotal',obtenerValorTotalProductosFecha);
 productoRouter.get('/buscar/:id',validarToken,buscarProducto);
 productoRouter.put('/actualizar/:id',validarToken, validatorProducto ,actualizarProducto);
 productoRouter.patch('/deshabilitar/:id', validarToken, deshabilitarProducto);
