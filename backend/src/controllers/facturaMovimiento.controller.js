75fdca3a7ea71fc2e1a7a1e73be51943a68dd952
AndersonMunoz <anderson180@hotmail.es>
2024-03-18 18:53:23 -0500

    Actualización de salida y entrada mejorados con validaciones

--- backend/src/controllers/facturaMovimiento.controller.js
+++ backend/src/controllers/facturaMovimiento.controller.js
@@ -467,12 +467,12 @@ export const actualizarMovimiento = async (req, res) => {
 			return res.status(400).json(error);
 		}
 		let id = req.params.id;
-		let { estado_producto_movimiento, precio_movimiento, nota_factura, fecha_caducidad,cantidad_peso_movimiento, fk_id_producto, fk_id_usuario} = req.body;
+		let { estado_producto_movimiento, precio_movimiento, nota_factura, fecha_caducidad,cantidad_peso_movimiento} = req.body;
 		let sqlPrev = `SELECT cantidad_peso_movimiento, num_lote FROM factura_movimiento WHERE id_factura=${id}`;
 		let resultPrev = await pool.query(sqlPrev,id);
 		let prevMovimiento = Number(resultPrev[0][0].cantidad_peso_movimiento);
 		let lote = Number(resultPrev[0][0].num_lote);
-		let sql = `UPDATE factura_movimiento SET estado_producto_movimiento='${estado_producto_movimiento}',precio_movimiento='${precio_movimiento}',nota_factura='${nota_factura}',fecha_caducidad='${fecha_caducidad}',cantidad_peso_movimiento='${cantidad_peso_movimiento}',fk_id_producto='${fk_id_producto}',fk_id_usuario='${fk_id_usuario}' where id_factura=${id}`;
+		let sql = `UPDATE factura_movimiento SET estado_producto_movimiento='${estado_producto_movimiento}',precio_movimiento='${precio_movimiento}',nota_factura='${nota_factura}',fecha_caducidad='${fecha_caducidad}',cantidad_peso_movimiento='${cantidad_peso_movimiento}'where id_factura=${id}`;
 
 		let sql2 = `UPDATE factura_movimiento SET precio_total_mov = cantidad_peso_movimiento * precio_movimiento, precio_movimiento = precio_movimiento WHERE id_factura=${id}`;
 		let diffMovimiento = cantidad_peso_movimiento - prevMovimiento;
@@ -480,7 +480,7 @@ export const actualizarMovimiento = async (req, res) => {
 		const [result1, result2,result3] = await Promise.all([
 			pool.query(sql, [estado_producto_movimiento,precio_movimiento, nota_factura,fecha_caducidad,cantidad_peso_movimiento]),
 			pool.query(sql2, [cantidad_peso_movimiento,precio_movimiento,id]),
-			pool.query(sql3, [fk_id_producto]),
+			pool.query(sql3),
 		]);
 		
 		if (result1[0].affectedRows > 0 && result2[0].affectedRows > 0  && result3[0].affectedRows > 0) {
@@ -503,17 +503,20 @@ export const actualizarMovimientoSalida = async (req, res) => {
 			return res.status(400).json(error);
 		}
 		let id = req.params.id;
-		let {nota_factura,cantidad_peso_movimiento} = req.body;
-		let sqlPrev = `SELECT cantidad_peso_movimiento,fk_id_producto FROM factura_movimiento WHERE id_factura=${id}`;
+		let {nota_factura,cantidad_peso_movimiento,destino_movimiento,fk_id_instructor,fk_id_titulado} = req.body;
+		let sqlPrev = `SELECT cantidad_peso_movimiento,fk_id_producto, num_lote FROM factura_movimiento WHERE id_factura=${id}`;
 		let resultPrev = await pool.query(sqlPrev,id);
 		let prevMovimiento = Number(resultPrev[0][0].cantidad_peso_movimiento);
 		let fk_id_producto = Number(resultPrev[0][0].fk_id_producto);
+		let num_lote = Number(resultPrev[0][0].num_lote);
 		let sql = `UPDATE factura_movimiento SET nota_factura='${nota_factura}',cantidad_peso_movimiento='${cantidad_peso_movimiento}' where id_factura=${id}`;
 		let diffMovimiento = cantidad_peso_movimiento - prevMovimiento;
-		let sql2 = `UPDATE productos SET cantidad_peso_producto = cantidad_peso_producto - ${diffMovimiento} WHERE id_producto=${fk_id_producto}`;
+		let sql2 = `UPDATE productos SET cantidad_peso_producto = cantidad_peso_producto - ${diffMovimiento}  WHERE num_lote='${num_lote}'`;
+		let sql3 = `UPDATE detalles SET destino_movimiento = '${destino_movimiento}', fk_id_instructor = '${fk_id_instructor}', fk_id_titulado = '${fk_id_titulado}'  WHERE fk_id_movimiento=${id}`;
 		const [result1, result2] = await Promise.all([
 			pool.query(sql, [nota_factura,cantidad_peso_movimiento,id]),
 			pool.query(sql2, [fk_id_producto]),
+			pool.query(sql3),
 		]);
 		
 
--- backend/src/validation/facturaMovimiento.validator.js
+++ backend/src/validation/facturaMovimiento.validator.js
@@ -3,7 +3,7 @@ import {check} from 'express-validator';
 export const validarFacturaMovimiento = [
     check('cantidad_peso_movimiento','Ingrese una cantidad válida, mayor a 0').isNumeric().custom(value => value > 0).notEmpty(),
     check('precio_movimiento','Ingrese un precio válido, mayor a 0').isNumeric().custom(value => value > 0).notEmpty(),
-    check('estado_producto_movimiento','Ingrese un estado válido en minúscula. Valores válidos: bueno, regular y malo').matches(/^(optimo|deficiente)$/).notEmpty(),
+    check('estado_producto_movimiento','Ingrese un estado válido en minúscula. Valores válidos: optimo y deficiente').matches(/^(optimo|deficiente)$/).notEmpty(),
     check('nota_factura','Ingrese un descripción válida, máximo 300 caracteres y mínimo 3').isLength({min: 3,max:300}).notEmpty(),
 /*     check('fk_id_producto','Ingrese un ID válido, mayor a 0').isNumeric().custom(value => value > 0).notEmpty(), */
     check('fk_id_usuario','Ingrese un ID válido, mayor a 0').isNumeric().custom(value => value > 0).notEmpty(),
@@ -22,7 +22,7 @@ export const validarFacturaMovimientoSalida = [
 
 
 export const validarFacturaMovimientoActu = [
-    check('estado_producto_movimiento','Ingrese un estado válido en minúscula. Valores válidos: bueno, regular y malo').matches(/^(optimo|deficiente)$/).notEmpty(),
+    check('estado_producto_movimiento','Ingrese un estado válido en minúscula. Valores válidos: optimo y deficiente').matches(/^(optimo|deficiente)$/).notEmpty(),
     check('cantidad_peso_movimiento','Ingrese una cantidad válida, mayor a 0').isNumeric().custom(value => value > 0).notEmpty(),
     check('nota_factura','Ingrese un descripción válida, máximo 300 caracteres y mínimo 3').isLength({min: 3,max:300}).notEmpty(),
     check('precio_movimiento','Ingrese un precio válido, mayor a 0').isNumeric().custom(value => value > 0).notEmpty(),
@@ -32,5 +32,8 @@ export const validarFacturaMovimientoActu = [
 export const validarFacturaMovimientoActuSalida = [
     check('nota_factura','Ingrese un descripción válida, máximo 300 caracteres y mínimo 3').isLength({min: 3,max:300}).notEmpty(),
     check('cantidad_peso_movimiento','Ingrese una cantidad válida, mayor a 0').isNumeric().custom(value => value > 0).notEmpty(),
+    check('fk_id_instructor','Ingrese un ID válido, mayor a 0').isNumeric().custom(value => value > 0).notEmpty(),
+    check('fk_id_titulado','Ingrese un ID válido, mayor a 0').isNumeric().custom(value => value > 0).notEmpty(),
+    check('destino_movimiento','Ingrese un estado válido en minúscula. Valores válidos: taller, produccion o evento').matches(/^(taller|produccion|evento)$/).notEmpty(),
     /* check('num_lote','Ingrese un número de lote válido, mayor a 0').isNumeric().custom(value => value > 0).notEmpty(), */
 ]
