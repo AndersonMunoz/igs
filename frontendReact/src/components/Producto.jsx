9bbff7d38307f305e4e33ca8125c737382f19923
karenviviana1234 <karenvivianadiazguevara@gmail.com>
2024-03-20 09:41:41 -0500

    validationsPRoveedor

--- frontendReact/src/components/Producto.jsx
+++ frontendReact/src/components/Producto.jsx
@@ -149,7 +149,6 @@ const Producto = () => {
 		}
 	}, [productos]);
   useEffect(() => {
-    
     window.onpopstate = function(event) {
       window.location.reload();
     }
@@ -158,8 +157,6 @@ const Producto = () => {
   useEffect(() => {
     setUserRoll(dataDecript(localStorage.getItem("roll")));
       listarProducto();
-      listarUp();
-      listarTipo();
   }, []); 
   // Función para resetear el estado del formulario
   const resetFormState = () => {
@@ -209,110 +206,6 @@ const Producto = () => {
         setProductos(data);
       }
     })
-  }
-    // Función para listar los tipos de producto
-  function listarTipo(){
-    fetch(`http://${portConexion}:3000/tipo/listarActivo`,{
-      method: "GET",
-      headers:{
-        "Content-type": "application/json",
-        token: localStorage.getItem("token"),
-      },
-    })
-    .then((res) => {
-      if (res.status === 204) {
-        return null;
-      }
-      return res.json();
-    })
-    .then((data) => {
-      if (data !== null) {
-        setTipo(data);
-      }
-    })
-    .catch((e) => {
-      console.error("Error al procesar la respuesta:", e);
-    });
-  }
-    // Función para listar las unidades productivas
-  function listarUp() {
-    fetch(`http://${portConexion}:3000/up/listar`, {
-      method: "GET",
-      headers: {
-        "Content-type": "application/json",
-        token: localStorage.getItem("token"),
-      },
-    })
-      .then((res) => {
-        if (res.status === 204) {
-          return null;
-        }
-        return res.json();
-      })
-      .then((data) => {
-        if (data !== null) {
-          setUp(data);
-        }
-      })
-      .catch((e) => {
-        console.error("Error al procesar la respuesta:", e);
-      });
-  }
-    // Función para manejar el cambio en la selección del tipo de producto
-  const handleTipo = (selectedOption) => {
-    setSelectedTipo(selectedOption); 
-  };
-    // Función para manejar el cambio en la selección de la unidad productiva
-  const handleUp = (selectedOption) => {
-    setSelectedUp(selectedOption);
-  };
-    // Función para registrar un nuevo producto
-  function registrarProducto() {
-    const descripcion_producto = document.getElementById('descripcion_producto').value;
-    Validate.validarCampos('.form-empty');
-    const validacionExitosa = Validate.validarSelect('.form-empt');
-    
-    if (!validacionExitosa) {
-      Sweet.registroFallido();
-      return;
-    }
-  
-    fetch(`http://${portConexion}:3000/producto/registrar`, {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json",
-        token: localStorage.getItem("token"),
-      },
-      body: JSON.stringify({ descripcion_producto, fk_id_up: selectedUp.value, fk_id_tipo_producto: selectedTipo.value }),
-    })
-    .then((res) => res.json())
-    .then(data => {
-      if (data.status === 200) {
-        Sweet.exito(data.message);
-        if ($.fn.DataTable.isDataTable(tableRef.current)) {
-          $(tableRef.current).DataTable().destroy();
-        }
-        listarProducto();
-        setShowModal(false);
-        removeModalBackdrop();
-        const modalBackdrop = document.querySelector('.modal-backdrop');
-        if (modalBackdrop) {
-          modalBackdrop.remove();
-        }
-      } else if (data.status === 403) {
-        Sweet.error(data.error.errors[0].msg);
-      } else if(data.status === 409){
-        Sweet.error(data.message);
-        return;
-      } else {
-        console.error('Error en la petición:', data);
-        Sweet.error('Hubo un error al registrar el producto.');
-      }
-    })
-    .catch(error => {
-      console.error('Error:', error);
-      Sweet.error('Hubo un error al registrar el producto.');
-    });
   }
     // Función para editar un producto
   function editarProducto(id) {
@@ -425,24 +318,6 @@ const Producto = () => {
   return (
     <div>
       <div className="boxBtnContendidoTitulo">
-        <div className="btnContenido1">
-          <button
-              type="button"
-              id="modalProducto"
-              className="btn-color btn mb-4"
-              data-bs-toggle="modal"
-              data-bs-target="#staticBackdrop"
-              onClick={() => {
-                setShowModal(true);
-                Validate.limpiar(".limpiar");
-                resetFormState();
-                setSelectedTipo(null);
-                setSelectedUp(null);
-              }}
-            >
-              Registrar Nuevo Producto
-            </button>
-        </div>
         <div className="btnContenido22">
           <h2 className="tituloHeaderpp">Lista de Productos</h2>
         </div>
@@ -555,68 +430,6 @@ const Producto = () => {
             )}
           </tbody>
         </table>
-      </div>
-
-      <div className="modal fade"data-bs-keyboard="false"id="staticBackdrop"tabIndex="-1"aria-labelledby="staticBackdropLabel"aria-hidden="true" data-bs-backdrop="static" ref={modalProductoRef} style={{ display: showModal ? 'block' : 'none' }} >
-        <div className="modal-dialog modal-dialog-centered d-flex align-items-center">
-          <div className="modal-content">
-            <div className="modal-header bg txt-color">
-              <h1 className="modal-title fs-5">Registrar Producto</h1>
-                <button type="button" className="btn-close text-white bg-white" data-bs-dismiss="modal"></button>
-            </div>
-            <div className="modal-body">
-              <form>
-                <div className="row mb-3">
-                  <div className="col-md-6">
-                    <label htmlFor="fk_id_tipo_producto" className="label-bold mb-2">Tipo Producto</label>
-                    <Select
-                        className="react-select-container  form-empt my-custom-class"
-                        classNamePrefix="react-select"
-                        options={tipos.map(element => ({ value: element.id, label: element.NombreProducto}))}
-                        placeholder="Selecciona..."
-                        onChange={handleTipo}
-                        value={selectedTipo}
-                        id="fk_id_tipo_producto"
-                      />
-                    <div className="invalid-feedback is-invalid">
-                      Por favor, seleccione un tipo de producto.
-                    </div>
-                  </div>
-                  <div className="col-md-6">
-                    <label htmlFor="fk_id_up" className="label-bold mb-2">Bodega</label>
-                    <Select
-                        className="react-select-container  form-empt my-custom-class"
-                        classNamePrefix="react-select"
-                        options={up.map(element => ({ value: element.id_up, label: element.nombre_up}))}
-                        placeholder="Selecciona..."
-                        onChange={handleUp}
-                        value={selectedUp}
-                        id="fk_id_up"
-                      />
-                    <div className="invalid-feedback is-invalid">
-                      Por favor, seleccione una unidad de peso.
-                    </div>
-                  </div>
-                </div>
-                <div className="mb-3">
-                  <label htmlFor="descripcionProducto" className="label-bold mb-2">Descripción</label>
-                  <textarea className="form-control form-empty limpiar" id="descripcion_producto" placeholder="Descripción del Producto" name="descripcion_producto" rows="4"></textarea>
-                  <div className="invalid-feedback is-invalid">
-                    Por favor, ingrese una descripción del producto.
-                  </div>
-                </div>
-              </form>
-            </div>
-            <div className="modal-footer">
-              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={() => { resetFormState();}}>
-                Cerrar
-              </button>
-              <button type="button" className="btn btn-color" onClick={registrarProducto}>
-                Registrar
-              </button>
-            </div>
-          </div>
-        </div>
       </div>
 
       <div className="modal fade"data-bs-keyboard="false"id="staticBackdrop2"tabIndex="-1"aria-labelledby="staticBackdropLabel"aria-hidden="true" data-bs-backdrop="static" ref={modalUpdateRef} style={{display:updateModal ? 'block' : 'none' }}>
