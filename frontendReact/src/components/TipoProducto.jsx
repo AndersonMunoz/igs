aad6aba10242ec4ada91cdafb46c47ee0bbecee5
jairola11 <jairostivenla11@gmail.com>
2024-03-20 10:07:01 -0500

    hola

--- frontendReact/src/components/TipoProducto.jsx
+++ frontendReact/src/components/TipoProducto.jsx
@@ -155,7 +155,7 @@ const Tipo = () => {
           [10, 50, 100, -1],
           ["10 Filas", "50 Filas", "100 Filas", "Ver Todo"],
         ],
-        order: [[2, "asc"]],
+     
       });
     }
   }, [tipos]);
